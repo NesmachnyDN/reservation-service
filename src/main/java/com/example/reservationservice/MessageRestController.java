d36a2055c14a9a4e1974ab5d581ae8360942817b
Несмачный Д.Н <NesmachnyyDN@stn.transneft.ru>
2017-08-31 15:07:47 +0700

    22

---
+++ src/main/java/com/example/reservationservice/MessageRestController.java
@@ -0,0 +1,26 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.reservationservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestController {
+    @Value("${message}")   
+    private String msg;
+    
+    @RequestMapping("/message")
+    String Message() {
+        return this.msg;
+    }
+    
+    
+}
